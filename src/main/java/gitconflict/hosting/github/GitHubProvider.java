af6e3398aa43c1fdaa22055af28b879f247320e6
Yurii Oliinyk <oliinykyurii123@gmail.com>
2025-03-27 00:08:04 +0100

    feat(GitHubProvider): add check for invalid input parameters in getChangedFilesSinceMergeBase

--- src/main/java/gitconflict/hosting/github/GitHubProvider.java
+++ src/main/java/gitconflict/hosting/github/GitHubProvider.java
@@ -9,7 +9,10 @@ import java.util.List;
 
 public class GitHubProvider implements VCSHostingProvider {
     @Override
-    public List<String> getChangedFilesSinceMergeBase(String owner, String repo, String accessToken, String mergeBase, String branch) throws URISyntaxException {
+    public List<String> getChangedFilesSinceMergeBase(String owner, String repo, String accessToken, String mergeBase, String branch) throws URISyntaxException, IllegalArgumentException {
+        if (owner == null || owner.isEmpty() || repo == null || repo.isEmpty() || accessToken == null || accessToken.isEmpty() || mergeBase == null || mergeBase.isEmpty() || branch == null || branch.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input parameters");
+        }
         GithubApiClient githubApiClient = new GithubRestClient();
         return githubApiClient.fetchChangedFiles(owner, repo, accessToken, mergeBase, branch);
     }
